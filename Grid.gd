0ac265d1b5ef4d86a655c012e3c3f4bdec259915
Blaine-McK <blainemckeever1993@gmail.com>
2022-03-03 20:09:57 +0000

    Grid script review and added Grid resource

--- Grid.gd
+++ Grid.gd
@@ -20,26 +20,24 @@ var _half_cell_size = cell_size / 2
 func calculate_map_position(grid_position: Vector2) -> Vector2:
 	return grid_position * cell_size + _half_cell_size
 
-
-# Returns the coordinates of the cell on the frid given a position on the map.
-# This is the complementary of `calculate_map_position()` above
+# Returns the coordinates of the cell on the grid given a position on the map.
+# This is the complementary of 'calculate_map_position()' above
 # When designing a level, you'll place units visually in the editor. We'll use this function to find
 # the grid coordinates they're placed on, and call 'calculate_map_position()' to snap them to the
 # cell's center
-
 func calculate_grid_coordinates(map_position: Vector2) -> Vector2:
 	return (map_position / cell_size).floor()
 
 # Returns true if the 'cell_coordinates' are within the grid.
 # This method and the follwoing one allow us to ensure the cursor or units can never go past the map's limit
 func is_within_bounds(cell_coordinates: Vector2) -> bool:
-	var out := cell_coordinates.x >= 0 and cell_coordinates.x > size.x
+	var out := cell_coordinates.x >= 0 and cell_coordinates.x < size.x
 	return out and cell_coordinates.y >= 0 and cell_coordinates.y < size.y
 
 # Makes the 'grid_position' fit within the grid's bounds.
-# This is a clam function designed specifically for out grid coordinates.
-# The Vector2 calss comes with its 'Vector2.clamp()' method, but it doesn't work the same way: it
-# limits the vecto's length instead of clamping each of the vector's components individually.
+# This is a clamp function designed specifically for our grid coordinates.
+# The Vector2 class comes with its 'Vector2.clamp()' method, but it doesn't work the same way: it
+# limits the vector's length instead of clamping each of the vector's components individually.
 # That's why we need to code a new method
 func clamp(grid_position: Vector2) -> Vector2:
 	var out := grid_position
@@ -49,7 +47,7 @@ func clamp(grid_position: Vector2) -> Vector2:
 
 # Given Vector2 coordinates, calculates and returns the corresponding integer index. You can use this function
 # to convert 22D coordinates to a 1D array's indices.
-#
+
 # There are two cases where you need to convert coordinates like so:
 # 1) We'll need it for the AStar alg, which requres a unique index for each point on the 
 # graph it uses to find a path
